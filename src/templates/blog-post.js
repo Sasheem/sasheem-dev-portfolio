b92aabd087cff2af2962f61dc168422697c23e72
Sasheem Snell <sasheem.snell@gmail.com>
2021-03-23 16:49:08 -0400

    added first blog post

---
+++ src/templates/blog-post.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { Helmet } from "react-helmet"
+import { graphql } from "gatsby"
+import styled from 'styled-components';
+
+// import '../css/blog-post.css'; // make it pretty!
+const Container = styled.div`
+    background-color: pink;
+`;
+const Post = styled.div``;
+const Content = styled.div``;
+
+export default function Template({
+  data // this prop will be injected by the GraphQL query we'll write in a bit
+}) {
+  const { markdownRemark: post } = data // data.markdownRemark holds your post data
+  return (
+    <Container>
+      <Helmet title={`Your Blog Name - ${post.frontmatter.title}`} />
+      <Post>
+        <h1>{post.frontmatter.title}</h1>
+        <Content
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+      </Post>
+    </Container>
+  )
+}
+
+export const pageQuery = graphql`
+  query BlogPostByPath($path: String!) {
+    markdownRemark(frontmatter: { path: { eq: $path } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        path
+        title
+      }
+    }
+  }
+`
