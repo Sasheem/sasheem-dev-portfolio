994571f0ebb0de1ebd7eafa246eb26b4ff23f838
Sasheem Snell <sasheem.snell@gmail.com>
2021-03-25 12:04:26 -0400

    removed unneeded code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
 // local components
 import GithubIcon from '../assets/github-brands.svg';
@@ -90,25 +88,5 @@ const IndexPage = () => (
       <Contact />
     </Layout>
 );
-
-// const IndexPage = () => (
-//   <Layout>
-//     <SEO title="Home" />
-//     <h1>Hi people</h1>
-//     <p>Welcome to your new Gatsby site.</p>
-//     <p>Now go build something great.</p>
-//     <StaticImage
-//       src="../images/gatsby-astronaut.png"
-//       width={300}
-//       quality={95}
-//       formats={["AUTO", "WEBP", "AVIF"]}
-//       alt="A Gatsby astronaut"
-//       style={{ marginBottom: `1.45rem` }}
-//     />
-//     <p>
-//       <Link to="/page-2/">Go to page 2</Link> <br />
-//     </p>
-//   </Layout>
-// )
 
 export default IndexPage
