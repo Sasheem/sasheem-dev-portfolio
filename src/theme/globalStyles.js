6b77a3770dcbd808816c1127803beacb2b5a2a08
Sasheem Snell <sasheem.snell@gmail.com>
2021-08-11 13:29:30 -0400

    changed text color

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -6,6 +6,9 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     font-family: -apple-system, Roboto, sans-serif, serif;
   }
+  h1, h2, h3, h4, h5, h6, p, a {
+    color: #44566C;     // dark blue grey
+  }
   p {
     font-size: 1.1em;
   }
