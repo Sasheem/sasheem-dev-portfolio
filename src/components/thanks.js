a74db57fcf06998a19d1fa338c899044f491f3f5
Sasheem Snell <sasheem.snell@gmail.com>
2022-02-14 16:26:28 -0500

    moved page to correct location

--- src/components/thanks.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Layout from '../layout'
-
-export default () => (
-  <Layout>
-    <h1>Thank you!</h1>
-    <p>I will get back to you shortly.</p>
-  </Layout>
-)
---
+++ src/pages/thanks.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// local components
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const Thanks = () => (
+  <Layout>
+    <SEO title="Thanks" />
+    <h1>Thank you!</h1>
+    <p>I will get back to you shortly.</p>
+  </Layout>
+)
+
+export default Thanks;
