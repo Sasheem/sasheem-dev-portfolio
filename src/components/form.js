337f2d9314cf097ffafd77fd82a58903097d08c9
Sasheem Snell <sasheem.snell@gmail.com>
2022-02-14 17:34:10 -0500

    removed unused code

--- src/components/form.js
+++ src/components/form.js
@@ -2,11 +2,9 @@ import React, { useState, useRef } from 'react';
 import { navigate } from 'gatsby';
 import styled from 'styled-components';
 import Recaptcha from 'react-google-recaptcha';
-import { useForm } from 'react-hook-form';
 
 // local components
 import { Button } from './common';
-import ContactSuccess from './contactSuccess';
 
 // styled components
 const ContactForm = styled.form`
@@ -53,16 +51,12 @@ const Textarea = styled.textarea`
     font-size: 0.9em;
     line-height: 1.25;
 `;
-const ErrorText = styled.p`
-    color: red;
-`;
 
 function encode(data) {
-    return Object.keys(data)
-      .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-      .join('&')
-  }
-  
+return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
 
 const Form = () => {
     const [state, setState] = useState({});
@@ -86,194 +80,70 @@ const Form = () => {
           .catch((error) => alert(error))
       }
 
-    const [submitted, setSubmitted] = useState(false);
-    const [formValues, setFormValues] = useState({
-        name: '',
-        email: '',
-        phone: '',
-        message: '',
-    });
-    // const recaptchaRef = useRef();
-    // const { 
-    //     register, 
-    //     handleSubmit, 
-    //     errors, 
-    //     reset, 
-    //     setError, 
-    //     formState: { isSubmitting},
-    // } = useForm();
-    // const GATEWAY_URL = 'https://dff7228u2k.execute-api.us-east-1.amazonaws.com/prod';
-    // const GATEWAY_URL = process.env.AWS_GATEWAY_URL;
-
-    // handle submit event
-    // error check & submit form w/ lambda function
-    // const onSubmit = async data => {
-        
-    //     // perform recaptcha check
-    //     // const token = await recaptchaRef.current.executeAsync();
-    //     const recaptchaValue = recaptchaRef.current.getValue(); 
-
-    //     // perform fetch request to gateway api to invoke lambda function with form data
-    //     try {
-            
-    //         if (recaptchaValue !== '') {
-    //             console.log(`recaptcha: ${recaptchaValue}:${typeof recaptchaValue}`);
-    //             await fetch(process.env.GATSBY_AWS_GATEWAY, {
-    //                 method: 'POST',
-    //                 mode: 'cors',
-    //                 cache: 'no-cache',
-    //                 body: JSON.stringify(data),
-    //                 headers: {
-    //                 'Content-type': 'application/json; charset=UTF-8',
-    //                 },
-    //             });
-
-    //             // reset form data upon successful submit
-    //             reset();
-    //             setFormValues({
-    //                 name: '',
-    //                 email: '',
-    //                 phone: '',
-    //                 message: '',
-    //             });
-    //             setSubmitted(true);
-    //         } else {
-    //             setError('submit', 'submitError', `Please toggle recaptcha`);
-    //             setSubmitted(false);
-    //         }
-            
-            
-    //       } catch (error) {
-    //         // handle server errors
-    //         setError('submit', 'submitError', `Doh! ${error.message}`);
-    //         setSubmitted(false);
-    //       }
-        
-    //   };
-
-    // update input on change
-    // const handleChange = ev => {
-    //     ev.persist();
-    //     setFormValues(currentValues => ({
-    //         ...currentValues,
-    //         [ev.target.name]: ev.target.value,
-    //     }));
-    // };
-
-    // handle recaptcha change
-    const onRecaptchaChange = (value) => {
-        console.log(`Captcha value: ${value}`);
-    };
-
-    // Component: Thank you message
-    const showThankYou = <ContactSuccess setSubmitted={setSubmitted} />;
-
     // Component: Form
-    const showForm = 
-    // <ContactForm onSubmit={handleSubmit(onSubmit)} method="post">
-    <ContactForm 
-        name="contact-sdev" 
-        method="POST"
-        action="/thanks/"
-        data-netlify-honeypot="bot-field"
-        data-netlify="true"
-        onSubmit={handleSubmit}
-    >
-        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
-        <input type="hidden" name="form-name" value="contact-sdev" />
-        <p hidden>
-          <label>
-            Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-          </label>
-        </p>
-        <Row>
-            <Label htmlFor='name'>Name</Label>
-            <Input 
-                type="text"  
-                name="name" 
-                placeholder="Name"
-                // disabled={isSubmitting}
-                // value={formValues.name} 
-                onChange={handleChange} 
-                // ref={register({
-                //     required: 'Name is required',
-                // })} 
-            />
-            {/* {errors.name && errors.name.message && <ErrorText>{errors.name.message}</ErrorText>} */}
-        </Row>
-        <DoubleRow>
-            <Row>
-                <Label htmlFor='email'>Email</Label>
-                <Input 
-                    type="text" 
-                    name="email" 
-                    placeholder="jon.email@gmail.com"
-                    // disabled={isSubmitting} 
-                    // value={formValues.email} 
-                    onChange={handleChange} 
-                    // ref={register({
-                    //     required: 'Email is required',
-                    //     pattern: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-                    // })}  
-                />
-                {/* {errors.email && errors.email.message && <ErrorText>{errors.email.message}</ErrorText>} */}
-            </Row>
-            <Row>
-                <Label htmlFor='phone'>Phone</Label>
-                <Input 
-                    type='tel'
-                    name='phone'
-                    placeholder='(555) - 555 - 5555'
-                    pattern='[0-9]{3}[0-9]{3}[0-9]{4}'
-                    // disabled={isSubmitting}
-                    // value={formValues.phone}
-                    onChange={handleChange}
-                    // ref={register({
-                    //     pattern: /[0-9]{3}[0-9]{3}[0-9]{4}/
-                    // })}
-                />
-            </Row>
-        </DoubleRow>
-        <Row>
-            <Label htmlFor='messageInput'>Message</Label>
-            <Textarea 
-                type="text" 
-                name="message" 
-                placeholder="Tell me about your project"
-                // disabled={isSubmitting}
-                rows="10" 
-                // value={formValues.message} 
-                onChange={handleChange} 
-                // ref={register({
-                //     required: 'Message is required',
-                // })}
-            />
-            {/* {errors.message && errors.message.message && <ErrorText>{errors.message.message}</ErrorText>} */}
-        </Row>
-        {/* <Row>
-            <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
-        </Row> */}
-        <Button type="submit">
-            Send 
-        </Button>
-        {/* <Row>
-            <ReCAPTCHA
-                ref={recaptchaRef}
-                sitekey={process.env.GATSBY_SITE_KEY}
-                size="normal"
-                onChange={onRecaptchaChange}
-            />
-        </Row> */}
-        {/* {errors.submit && errors.submit.message &&
-            <Row>
-                <ErrorText>{errors.submit.message}</ErrorText>
-            </Row>
-        } */}
-    </ContactForm>;
- 
     return (
-        showForm
-        // submitted ? showThankYou : showForm
+        <ContactForm 
+            name="contact-sdev" 
+            method="POST"
+            action="/thanks/"
+            data-netlify-honeypot="bot-field"
+            data-netlify="true"
+            onSubmit={handleSubmit}
+        >
+            {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
+            <input type="hidden" name="form-name" value="contact-sdev" />
+            <p hidden>
+            <label>
+                Don’t fill this out: <input name="bot-field" onChange={handleChange} />
+            </label>
+            </p>
+            <Row>
+                <Label htmlFor='name'>Name</Label>
+                <Input 
+                    type="text"  
+                    name="name" 
+                    placeholder="Name"
+                    onChange={handleChange} 
+                />
+            </Row>
+            <DoubleRow>
+                <Row>
+                    <Label htmlFor='email'>Email</Label>
+                    <Input 
+                        type="text" 
+                        name="email" 
+                        placeholder="jon.email@gmail.com"
+                        onChange={handleChange} 
+                    />
+                </Row>
+                <Row>
+                    <Label htmlFor='phone'>Phone</Label>
+                    <Input 
+                        type='tel'
+                        name='phone'
+                        placeholder='(555) - 555 - 5555'
+                        pattern='[0-9]{3}[0-9]{3}[0-9]{4}'
+                        onChange={handleChange}
+                    />
+                </Row>
+            </DoubleRow>
+            <Row>
+                <Label htmlFor='messageInput'>Message</Label>
+                <Textarea 
+                    type="text" 
+                    name="message" 
+                    placeholder="Tell me about your project"
+                    rows="10" 
+                    onChange={handleChange} 
+                />
+            </Row>
+            {/* <Row>
+                <Recaptcha ref={recaptchaRef} sitekey={RECAPTCHA_KEY} />
+            </Row> */}
+            <Button type="submit">
+                Send 
+            </Button>
+        </ContactForm>
     );
 };
 
