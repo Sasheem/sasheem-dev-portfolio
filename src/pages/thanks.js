2ab2e964577bacb748fe9b53ec66a47621b4d352
Sasheem Snell <sasheem.snell@gmail.com>
2022-02-14 19:19:50 -0500

    made component reusable

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import styled from 'styled-components';
 
 // local components
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Social from '../components/social';
-
-const Content = styled.div`
-	margin-top: 144px;
-	margin-bottom: 144px;
-	color: #44566C;     // dark blue grey
-	display: flex;
-	flex-direction: column;
-
-  // screen larger than 768px
-  @media only screen and (min-width: 768px) {
-      margin-top: 256px;
-      margin-bottom: 336px;
-      align-items: center;
-      justify-content: center;
-  }
-`;
+import { Content } from '../components/common';
 
 const Thanks = () => (
   <Layout>
